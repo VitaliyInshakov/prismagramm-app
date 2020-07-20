b2effa19a7015e708fe800b79e58537afc37b54a
Vitaliy <michael8913@gmail.com>
2020-07-20 08:55:09 +0300

    preloading cache

--- App.js
+++ App.js
@@ -1,32 +1,52 @@
 import React, { useState, useEffect } from "react";
+import { Text, View } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
-import { Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { ApolloProvider } from "react-apollo-hooks";
+import apolloClientOptions from "./apollo";
 
 export default function App() {
-  const [loaded, setLoaded] = useState(false);
-  const preload = async () => {
-      try {
-        await Font.loadAsync({
-          ...Ionicons.font,
+    const [loaded, setLoaded] = useState(false);
+    const [client, setClient] = useState(null);
+
+    const preload = async () => {
+        try {
+            await Font.loadAsync({
+            ...Ionicons.font,
         });
-        await Asset.loadAsync(require("./assets/logo.png"));
-        setLoaded(true);
-      } catch (e) {
-        console.log(e);
-      }
-  };
+            await Asset.loadAsync(require("./assets/logo.png"));
+            const cache = new InMemoryCache();
+            await persistCache({
+                cache,
+                storage: AsyncStorage,
+            });
+            const client = new ApolloClient({
+                cache,
+                ...apolloClientOptions,
+            });
+            setLoaded(true);
+            setClient(client);
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
-  useEffect(() => {
-    preload();
-  }, []);
+    useEffect(() => {
+        preload();
+    }, []);
 
-  return (loaded ?
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-    : <AppLoading/>
-  );
+    return (loaded && client ?
+        <ApolloProvider client={client}>
+            <View>
+                <Text>Open up App.js to start working on your app!</Text>
+            </View>
+        </ApolloProvider>
+        : <AppLoading/>
+    );
 }
