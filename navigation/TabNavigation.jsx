bce767bc02c60f1b22b14e53e4ae841855d5f4d1
Vitaliy <michael8913@gmail.com>
2020-07-28 09:20:41 +0300

    login

--- navigation/TabNavigation.jsx
+++ navigation/TabNavigation.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { View } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createStackNavigator } from "react-navigation-stack";
 import Home from "../screens/Tabs/Home";
