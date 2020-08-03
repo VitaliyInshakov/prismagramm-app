40fe2055ffb016797110633fbc15d6cab293e2c4
Vitaliy <michael8913@gmail.com>
2020-08-03 09:28:27 +0300

    facebook login

--- screens/Auth/SignUp.jsx
+++ screens/Auth/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Alert, TouchableWithoutFeedback, Keyboard } from "react-native";
 import styled from "styled-components/native";
 import { useMutation } from "react-apollo-hooks";
+import * as Facebook from "expo-facebook";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
 import useInput from "../../hooks/useInput";
@@ -11,6 +12,14 @@ const View = styled.View`
   justify-content: center;
   align-items: center;
   flex: 1;
+`;
+
+const FBContainer = styled.View`
+  margin-top: 25px;
+  padding-top: 25px;
+  border-top-width: 1px;
+  border-color: ${props => props.theme.lightGreyColor};
+  border-style: solid;
 `;
 
 export default ({ navigation }) => {
@@ -61,6 +70,31 @@ export default ({ navigation }) => {
         }
     }
 
+    const fbLogin = async () => {
+        try {
+            setLoading(true);
+            await Facebook.initializeAsync("1049324578872281");
+
+            const { type, token } = await Facebook.logInWithReadPermissionsAsync({
+                permissions: ["public_profile"],
+            });
+            if (type === "success") {
+                const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=id,last_name,first_name,email`);
+                const { email, first_name, last_name } = await response.json();
+                emailInput.setValue(email);
+                fNameInput.setValue(first_name);
+                lNameInput.setValue(last_name);
+                const [username] = email.split("@");
+                usernameInput.setValue(username);
+                setLoading(false);
+            } else {
+                // type === 'cancel'
+            }
+        } catch ({ message }) {
+            alert(`Facebook Login Error: ${message}`);
+        }
+    }
+
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <View>
@@ -87,6 +121,14 @@ export default ({ navigation }) => {
                     autoCorrect={false}
                 />
                 <AuthButton loading={loading} onPress={handleSingUp} text="Sign up" />
+                <FBContainer>
+                    <AuthButton
+                        bgColor={"#2D4DA7"}
+                        loading={false}
+                        onPress={fbLogin}
+                        text="Connect Facebook"
+                    />
+                </FBContainer>
             </View>
         </TouchableWithoutFeedback>
     );
