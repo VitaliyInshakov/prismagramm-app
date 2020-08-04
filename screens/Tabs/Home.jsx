83ba5b9648c786d6e8a8f5624fd08a072c861e64
Vitaliy <michael8913@gmail.com>
2020-08-04 09:43:04 +0300

    apollo context

--- screens/Tabs/Home.jsx
+++ screens/Tabs/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components/native";
+import { useQuery } from "react-apollo-hooks";
+import { gql } from "apollo-boost";
 import Loader from "../../components/Loader";
 
 const View = styled.View`
@@ -8,8 +10,38 @@ const View = styled.View`
   flex: 1;
 `;
 
-export default () => (
-    <View>
-        <Loader />
-    </View>
-);
+const FEED_QUERY = gql`
+    {
+        seeFeed {
+            id
+            location
+            caption
+            user {
+                id
+                avatar
+                username
+            }
+            files {
+                id
+                url
+            }
+            likeCount
+            isLiked
+            comments {
+                id
+                text
+                user {
+                    id
+                    username
+                }
+            }
+            createdAt
+        }
+    }
+`;
+
+export default () => {
+    const { loading, data } = useQuery(FEED_QUERY);
+    console.log(loading, data);
+    return <View>{loading ? <Loader /> : null}</View>;
+};
